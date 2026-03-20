5e5d7204e214f8c2facfb8d61d2f2c36b274e246
Vojta Vávra <votisek@proton.me>
2026-03-20 22:31:55 +0100

    add correct file permissions to install

--- mcsarch/profiledef.sh
+++ mcsarch/profiledef.sh
@@ -18,6 +18,7 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/install"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
