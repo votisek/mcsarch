9617ecf7b650c8fe6bc8c3c364654650a6ba665c
Vojta Vávra <votisek@proton.me>
2026-03-24 12:33:00 +0100

    feat: jay config + install.sh

---
+++ Jay/install.sh
@@ -0,0 +1,14 @@
+USERNAME=$(arch-chroot /mnt awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/passwd | head -n1)
+
+arch-chroot /mnt usermod -aG seat,video,input $USERNAME
+arch-chroot /mnt mkdir -p /home/$USERNAME/.config
+arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME 
+
+gum style --bold "Copying Jay config to new system..."
+arch-chroot /mnt mkdir -p /home/$USERNAME/.config/jay
+arch-chroot /mnt mkdir -p /home/$USERNAME/.config/waybar
+cp /root/mcsarch/Jay/config.toml /mnt/home/$USERNAME/.config/jay
+
+/root/mcsarch/hyprlock/install.sh
+/root/mcsarch/waybar/install.sh
+/root/mcsarch/sddm/install.sh
