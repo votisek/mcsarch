338c420bdf1e16d0cb94ca7c592f0e967fb7cde6
Vojta Vávra <votisek@proton.me>
2026-04-05 12:52:27 +0200

    fix: paths is core and remove seat group in jay

--- Jay/install.sh
+++ Jay/install.sh
@@ -3,6 +3,7 @@ USERNAME=$(arch-chroot /mnt awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/p
 gum style --bold "Copying Jay config to new system..."
 arch-chroot /mnt mkdir -p /home/$USERNAME/.config/jay
 cp /root/mcsarch/Jay/config.toml /mnt/home/$USERNAME/.config/jay
+mkdir -p /mnt/usr/share/wayland-sessions
 cp /root/mcsarch/Jay/jay.desktop /mnt/usr/share/wayland-sessions/
 gum style --bold "Done!"
 
--- core.sh
+++ core.sh
@@ -2,10 +2,10 @@
 chmod +x /root/mcsarch/sddm/install.sh
 chmod +x /root/mcsarch/waywall/install.sh
 chmod +x /root/mcsarch/prismlauncher/install.sh
-./root/mcsarch/sddm/install.sh
-./root/mcsarch/waywall/install.sh
-./root/mcsarch/prismlauncher/install.sh
-arch-chroot /mnt usermod -aG seat,video,input $USERNAME
+/root/mcsarch/sddm/install.sh
+/root/mcsarch/waywall/install.sh
+/root/mcsarch/prismlauncher/install.sh
+arch-chroot /mnt usermod -aG video,input $USERNAME
 arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME 
 arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME/.local
 arch-chroot /mnt mkdir -p /home/$USERNAME/.config
@@ -15,7 +15,7 @@ if [ $1 != "Plasma" ]; then
     chmod +x /root/mcsarch/hyprlock/install.sh
     chmod +x /root/mcsarch/waybar/install.sh
     chmod +x /root/mcsarch/kitty/install.sh
-    ./root/mcsarch/hyprlock/install.sh
-    ./root/mcsarch/waybar/install.sh
-    ./root/mcsarch/kitty/install.sh
+    /root/mcsarch/hyprlock/install.sh
+    /root/mcsarch/waybar/install.sh
+    /root/mcsarch/kitty/install.sh
 fi
