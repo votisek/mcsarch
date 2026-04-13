d8cb6a1a1135054c87a1086b801d29fe5a00b61a
Vojta Vávra <votisek@proton.me>
2026-04-13 14:05:50 +0200

    style: add font SF-Pro-Rounded-Semibold to hyprlock

--- hyprlock/install.sh
+++ hyprlock/install.sh
@@ -2,4 +2,5 @@
 gum style --bold "Copying Hyprlock config to new system"
 arch-chroot /mnt mkdir -p /etc/skel/.config/hypr
 cp /root/mcsarch/hyprlock/hyprlock.conf /mnt/etc/skel/.config/hypr
+cp /root/mcsarch/hyprlock/fonts/* /mnt/usr/share/fonts
 gum style --bold "Done!"
