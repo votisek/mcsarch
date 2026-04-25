5e59b877c21b8336c329f1fcb6be22db272ef606
Vojta Vávra <votisek@proton.me>
2026-04-25 12:58:49 +0200

    fix: edit jay installation so its versioned through yay

--- Jay/install.sh
+++ Jay/install.sh
@@ -1,15 +1,5 @@
 gum style --bold "Installing Jay..."
-arch-chroot /mnt git clone https://aur.archlinux.org/jay-git.git /opt/jay
-arch-chroot /mnt cd /opt/jay/
-arch-chroot /mnt makepkg -si --noconfirm
-arch-chroot /mnt pacman -U *.tar.zst --noconfirm
-echo EOF<<
-ps=$(pwd)
-cd /opt/jay/
-makepkg -si --noconfirm
-pacman -U *.tar.zst --noconfirm
-cd $ps
-EOF
+arch-chroot /mnt yay -S --no-confirm jay-git
 
 gum style --bold "Copying Jay config to new system..."
 arch-chroot /mnt mkdir -p /etc/skel/.config/jay
