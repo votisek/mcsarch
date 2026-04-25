820868cbf937f5e87788bd34418e897a4694da4d
Vojta Vávra <votisek@proton.me>
2026-04-25 12:27:45 +0200

    fix: prismlauncher instance not registering + emptty instance gen

--- prismlauncher/install.sh
+++ prismlauncher/install.sh
@@ -4,7 +4,7 @@ gum style --bold "Setting up Prism Launcher..."
 arch-chroot /mnt mkdir -p /etc/skel/.local/share/PrismLauncher
 cp -r /root/mcsarch/prismlauncher/prismlauncher.cfg /mnt/etc/skel/.local/share/PrismLauncher/
 mkdir -p /mnt/etc/skel/.local/share/PrismLauncher/instances
-mkdir -p /mnt/etc/skel/.local/share/PrismLauncher/instances/MCSRRanked
-unzip /root/mcsarch/prismlauncher/MCSRRanked.zip -d /mnt/etc/skel/.local/share/PrismLauncher/instances/ &> /dev/null
+unzip /root/mcsarch/prismlauncher/MCSRRanked.zip -d /mnt/etc/skel/.local/share/PrismLauncher/instances/. &> /dev/null
+mv /mnt/etc/skel/.local/share/PrismLauncher/instances/
 
 gum style --bold "Prism Launcher setup complete!"
