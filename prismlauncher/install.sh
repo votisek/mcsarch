5eee275a61e98b628d0c5d9add04d682e7e1f897
Vojta Vávra <votisek@proton.me>
2026-05-10 11:22:13 +0200

    fix: another file path error with prism

--- prismlauncher/install.sh
+++ prismlauncher/install.sh
@@ -1,9 +1,8 @@
 
 gum style --bold "Setting up Prism Launcher..."
 
-arch-chroot /mnt mkdir -p /tmp/mcsarch_skel/.local/share/PrismLauncher
-cp -r /root/mcsarch/prismlauncher/prismlauncher.cfg /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/
 mkdir -p /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances
-unzip /root/mcsarch/prismlauncher/MCSRRanked.zip -d /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances/. &> /dev/null
+cp -r /root/mcsarch/prismlauncher/prismlauncher.cfg /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/
+unzip -o /root/mcsarch/prismlauncher/MCSRRanked.zip -d /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances/ &> /dev/null
 
 gum style --bold "Prism Launcher setup complete!"
