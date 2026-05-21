3378149af49296994b9a840bc8161c70809f800a
Vojta Vávra <votisek@proton.me>
2026-05-21 08:44:51 +0200

    fix: prismlauncher instance not having mods

--- prismlauncher/install.sh
+++ prismlauncher/install.sh
@@ -3,6 +3,6 @@ gum style --bold "Setting up Prism Launcher..."
 
 mkdir -p /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances
 cp -r /root/mcsarch/prismlauncher/prismlauncher.cfg /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/
-unzip -o /root/mcsarch/prismlauncher/MCSRRanked.zip -d /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances/ &> /dev/null
+tar -xf /root/mcsarch/prismlauncher/mcsrranked.tar.zst -C /mnt/tmp/mcsarch_skel/.local/share/PrismLauncher/instances/MCSRRanked &> /dev/null
 
 gum style --bold "Prism Launcher setup complete!"
